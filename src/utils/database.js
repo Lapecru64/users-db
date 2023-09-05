55bc779484f99ddf9bc3f4ebde451c746504f9f2
Lapecru64 <perlura64@outlook.com>
2023-09-04 21:29:32 -0600

    cambios users-db

--- src/utils/database.js
+++ src/utils/database.js
@@ -9,6 +9,10 @@ const db = new Sequelize({
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
 dialect: 'postgres',
+dialectOptions: {ssl: {required: true, rejectUnauthorized: false}}
 });
+
+// para local comentamos el dialect options
+
 
 export default db;
